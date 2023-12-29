0e82c686b0797e1d293b8a69cf6b7439e0d3ce5d
rvbear <rvbear2000@gmail.com>
2023-12-29 18:22:27 +0900

    feat:기압 초기화

--- src/main/java/com/example/controller/ElevatorController.java
+++ src/main/java/com/example/controller/ElevatorController.java
@@ -3,14 +3,13 @@ package com.example.controller;
 import com.example.entity.Elevator;
 import com.example.repository.ElevatorRepository;
 import com.example.service.ElevatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "예제 API", description = "Swagger 테스트용 API")
+@Tag(name = "ETA API", description = "Swagger 테스트용 API")
 @RestController
 @RequiredArgsConstructor
 public class ElevatorController {
@@ -18,7 +17,10 @@ public class ElevatorController {
     private final ElevatorService elevatorService;
     private final ElevatorRepository elevatorRepository;
 
-    @GetMapping("/update/{id}/{pressure}")
+    @Operation(summary = "엘리베이터의 기압 입력", description = "엘리베이터 번호와 기압을 입력받아 층수를 반환합니다.")
+    @Parameter(name = "id", description = "엘리베이터 번호")
+    @Parameter(name = "pressure", description = "엘리베이터의 기압")
+    @GetMapping("/eta/update/{id}/{pressure}")
     public Elevator updateElevatorPressure(@PathVariable("id") int id, @PathVariable("pressure") double pressure) {
         // 기압 차이를 이용하여 현재 층수 계산
         int floor = elevatorService.calculateFloorFromPressure(pressure);
@@ -29,5 +31,14 @@ public class ElevatorController {
         elevatorRepository.save(elevator);
 
         return elevator;
+    }
+
+    @Operation(summary = "기압 초기화", description = "1층의 기압을 통해 초기화를 시킵니다.")
+    @Parameter(name = "basePressure", description = "초기화 기압")
+    @Parameter(name = "pressureFloor", description = "층별 기압 차")
+    @PostMapping("/eta/init/{base}/{per}")
+    public String updateInit(@PathVariable("base") double basePressure, @PathVariable("per") double pressureFloor) {
+        // 받은 기압들을 토대로 초기화
+        return elevatorService.pressureInit(basePressure, pressureFloor) ? "초기화 성공!" : "오류 발생..";
     }
 }
--- src/main/java/com/example/service/ElevatorService.java
+++ src/main/java/com/example/service/ElevatorService.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class ElevatorService {
 
-    private static final double BASE_PRESSURE = 120.12;         // 기준 압력
-    private static final double PRESSURE_PER_FLOOR = 0.12;      // 층당 기압 변화 (임의로 설정)
+    private static double BASE_PRESSURE = 120.12;         // 기준 압력
+    private static double PRESSURE_PER_FLOOR = 0.12;      // 층당 기압 변화 (임의로 설정)
+
+    public boolean pressureInit(double basePressure, double pressureFloor) {
+        try {
+            // 넘어온 값들을 입력
+            BASE_PRESSURE = basePressure;
+            PRESSURE_PER_FLOOR = pressureFloor;
+            return true;
+        } catch (Exception e) {
+            // 오류 발생 시 false return
+            return false;
+        }
+    }
 
     public int calculateFloorFromPressure(double currentPressure) {
         // 기압 차이를 이용하여 층수를 계산
